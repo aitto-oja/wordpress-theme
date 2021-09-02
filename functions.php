b722e42ad7d58a4aef26e58011b552c6fee56642
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-02 15:02:01 +0300

    Add function to load font awesome

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function aittooja_files() {
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('aittooja_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('aittooja_extra_styles', get_theme_file_uri('/build/index.css'));
 }
