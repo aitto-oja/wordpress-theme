13101177b071217692481f93b1e8789659b8a82c
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-02 13:36:02 +0300

    Edit index.php to show posts

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-This is my custom theme index.php page. 
+<?php 
+while(have_posts()) {
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <p><?php the_content(); ?></p>
+    <hr>
+<?php }
