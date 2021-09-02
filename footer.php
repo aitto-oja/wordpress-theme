b04ffc29f5bd4eb38c913a1c2333556e26b89511
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-02 15:37:58 +0300

    Remove campuses from footer menu

--- footer.php
+++ footer.php
@@ -16,7 +16,6 @@
                             <li><a href="#">About Me</a></li>
                             <li><a href="#">Projects</a></li>
                             <li><a href="#">Events</a></li>
-                            <li><a href="#">Campuses</a></li>
                         </ul>
                     </nav>
                 </div>
