68c4226376b60fa466d9a4b5e575b6024033f7ee
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-02 23:56:23 +0300

    Replace hard coded values with generic values

--- page.php
+++ page.php
@@ -6,11 +6,11 @@ while(have_posts()) {
     the_post(); ?>
 
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/florian-krumm-keyboard.jpg'); ?>)"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">My History</h1>
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
             <div class="page-banner__intro">
-                <p>Get to know about my career.</p>
+                <p>REPLACE ME LATER.</p>
             </div>
         </div>
     </div>
@@ -22,17 +22,16 @@ while(have_posts()) {
             </p>
         </div>
 
-        <div class="page-links">
+        <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Me</a></h2>
             <ul class="min-list">
                 <li class="current_page_item"><a href="#">History</a></li>
                 <li><a href="#">Goals</a></li>
             </ul>
-        </div>
+        </div> -->
 
         <div class="generic-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+            <?php the_content(); ?>
         </div>
     </div>
 
