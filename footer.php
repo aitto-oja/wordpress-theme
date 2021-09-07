51f9a388827ba4287a7838088298bd13c5c2d7d7
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-07 11:09:23 +0300

    Create dynamic footer menus

--- footer.php
+++ footer.php
@@ -12,22 +12,22 @@
                 <div class="site-footer__col-two">
                     <h3 class="headline headline--small">Explore</h3>
                     <nav class="nav-list">
-                        <ul>
-                            <li><a href="<?php echo site_url('/about-me'); ?>">About Me</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Events</a></li>
-                        </ul>
+                        <?php 
+                            wp_nav_menu(array(
+                                'theme_location' => 'footerLocationOne', 
+                            ))
+                        ?>
                     </nav>
                 </div>
 
                 <div class="site-footer__col-three">
                     <h3 class="headline headline--small">Learn</h3>
                     <nav class="nav-list">
-                        <ul>
-                            <li><a href="#">Legal</a></li>
-                            <li><a href="<?php echo site_url('/privacy-policy'); ?>">Privacy</a></li>
-                            <li><a href="#">Careers</a></li>
-                        </ul>
+                        <?php 
+                            wp_nav_menu(array(
+                                'theme_location' => 'footerLocationTwo', 
+                            ))
+                        ?>
                     </nav>
                 </div>
             </div>
