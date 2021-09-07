d91cb3ed1ef5cc3b3d8a8b58190f7386cc1cd391
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-07 14:31:30 +0300

    Edit event post type labels

--- functions.php
+++ functions.php
@@ -24,6 +24,10 @@ function aittooja_post_types() {
         'public' => true, 
         'labels' => array(
             'name' => 'Events', 
+            'add_new_item' => 'Add New Event', 
+            'edit_item' => 'Edit Event', 
+            'all_items' => 'All Events', 
+            'singular_name' => 'Event', 
         ), 
         'menu_icon' => 'dashicons-calendar', 
     ));
