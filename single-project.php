cc6b4722d7aabd28bfad0ded4790c4aee1db903e
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-08 18:31:29 +0300

    Add related events to project page

--- single-project.php
+++ single-project.php
@@ -28,6 +28,58 @@ while(have_posts()) {
         <div class="generic-content">
             <?php the_content(); ?>
         </div>
+        
+        <?php 
+        $today = date('Ymd');
+        $relatedEvents = new WP_Query(array( 
+            'post_type' => 'event', 
+            'meta_key' => 'event_date', 
+            'orderby' => 'meta_value_num', 
+            'order' => 'ASC', 
+            'meta_query' => array(
+                array(
+                    'key' => 'event_date', 
+                    'compare' => '>=', 
+                    'value' => $today,
+                    'type'  => 'numeric', 
+                ), 
+                array(
+                    'key' => 'related_projects', 
+                    'compare' => 'LIKE',
+                    'value' => '"' . get_the_ID() . '"',  
+                ), 
+            ), 
+        ));
+
+        if ($relatedEvents) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Related Events</h2>';
+        }
+
+        while ($relatedEvents->have_posts()) {
+            $relatedEvents->the_post(); ?>
+            <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                    <span class="event-summary__month">
+                        <?php 
+                            $eventDate = new DateTime(get_field('event_date'));
+                            echo $eventDate->format('M');
+                        ?>
+                    </span>
+                    <span class="event-summary__day"><?php echo $eventDate->format('j'); ?></span>
+                </a>
+                <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <p>
+                        <?php if (has_excerpt()) {
+                            echo get_the_excerpt();
+                        } else {
+                            echo wp_trim_words(get_the_content(), 10);
+                        } ?> 
+                        <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                </div>
+            </div>
+        <?php } ?>
     </div>
 <?php }
 
