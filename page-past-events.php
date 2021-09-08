feab7b3bf8aaa2cd6b595e693c53d901ab900337
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-08 14:52:22 +0300

    Make pagination work for custom query

--- page-past-events.php
+++ page-past-events.php
@@ -16,6 +16,7 @@ get_header(); ?>
     <?php 
         $today = date('Ymd');
         $pastEvents = new WP_Query(array(
+            'paged' => get_query_var('paged', 1), 
             'post_type' => 'event', 
             'meta_key' => 'event_date', 
             'orderby' => 'meta_value_num', 
@@ -49,7 +50,9 @@ get_header(); ?>
             </div>
         <?php }
 
-        echo paginate_links();
+        echo paginate_links(array(
+            'total' => $pastEvents->max_num_pages, 
+        ));
     ?>
 </div>
 
