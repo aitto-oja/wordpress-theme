b4fe2235bf387c1e9ce74f9a5b4959f1395286d2
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-08 09:25:03 +0300

    Edit archive template for event post type

--- archive-event.php
+++ archive-event.php
@@ -5,13 +5,9 @@ get_header(); ?>
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/gray-satellite-disc-on-field-copy.jpg'); ?>)"></div>
     <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large">
-            <?php 
-                the_archive_title();
-            ?>
-        </h1>
+        <h1 class="headline headline--large">All Events</h1>
         <div class="page-banner__intro">
-            <p><?php the_archive_description(); ?></p>
+            <p>See what is going on.</p>
         </div>
     </div>
 </div>
@@ -20,15 +16,14 @@ get_header(); ?>
     <?php 
         while (have_posts()) {
             the_post(); ?>
-            <div class="post-item">
-                <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <div class="metabox">
-                    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('j.n.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
-                </div>
-
-                <div class="generic-content">
-                    <?php the_excerpt(); ?>
-                    <p><a class="btn btn--blue" href="<?php echo the_permalink(); ?>">Continue reading &raquo;</a></p>
+            <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                    <span class="event-summary__month">Mar</span>
+                    <span class="event-summary__day">25</span>
+                </a>
+                <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <p><?php echo wp_trim_words(get_the_content(), 10); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
                 </div>
             </div>
         <?php }
