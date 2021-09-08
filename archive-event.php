b86fb180602fc9985dabfdfe19dff9ef57791d31
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-08 14:58:09 +0300

    Add link to past events

--- archive-event.php
+++ archive-event.php
@@ -35,6 +35,13 @@ get_header(); ?>
 
         echo paginate_links();
     ?>
+
+    <hr class="section-break">
+
+    <p>
+        Looking for a recap of past events? 
+        <a href="<?php site_url('/past-events') ?>">Check out past events archive</a>. 
+    </p>
 </div>
 
 
