578356d6e5f61120fa5cac5303574634b7dd9155
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-08 12:14:09 +0300

    Show only upcoming events

--- functions.php
+++ functions.php
@@ -43,9 +43,18 @@ add_action('init', 'aittooja_post_types');
 
 function aittooja_adjust_queries($query) {
     if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+        $today = date('Ymd');
         $query->set('meta_key', 'event_date');
         $query->set('orderby', 'meta_value_num');
         $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+                'key' => 'event_date', 
+                'compare' => '>=', 
+                'value' => $today, 
+                'type' => 'numeric', 
+            ), 
+        ));
     }
 }
 
