ee626a5c88fd3f7c505c8d16e16103837975ddf5
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-20 14:02:49 +0300

    Modify custom api url

--- inc/search-route.php
+++ inc/search-route.php
@@ -9,7 +9,60 @@ function aittoojaRegisterSearch() {
     ));
 }
 
-function aittoojaSearchResults() {
-    return 'congrats';
+function aittoojaSearchResults($data) {
+    $mainQuery = new WP_Query(array(
+        'post_type' => array('post', 'page', 'language', 'project', 'framework', 'event'), 
+        's' => sanitize_text_field($data['term']), 
+    ));
+
+    $results = array(
+        'generalInfo' => array(), 
+        'languages' => array(), 
+        'projects' => array(), 
+        'events' => array(), 
+        'frameworks' => array(), 
+    );
+
+    while($mainQuery->have_posts()) {
+        $mainQuery->the_post();
+        
+        if (get_post_type() == 'post' OR get_post_type() == 'page') {
+            array_push($results['generalInfo'], array(
+                'title' => get_the_title(), 
+                'permalink' => get_the_permalink(), 
+            ));
+        }
+        
+        if (get_post_type() == 'language') {
+            array_push($results['languages'], array(
+                'title' => get_the_title(), 
+                'permalink' => get_the_permalink(), 
+            ));
+        }
+
+        if (get_post_type() == 'project') {
+            array_push($results['projects'], array(
+                'title' => get_the_title(), 
+                'permalink' => get_the_permalink(), 
+            ));
+        }
+
+        if (get_post_type() == 'framework') {
+            array_push($results['frameworks'], array(
+                'title' => get_the_title(), 
+                'permalink' => get_the_permalink(), 
+            ));
+        }
+
+        if (get_post_type() == 'event') {
+            array_push($results['events'], array(
+                'title' => get_the_title(), 
+                'permalink' => get_the_permalink(), 
+            ));
+        }
+
+    }
+
+    return $results;
 }
 
