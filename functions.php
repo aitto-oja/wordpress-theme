0d244e753262f2b0211f393f0cc3290db820c4de
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-20 11:06:12 +0300

    Add wp function to localize a script with data

--- functions.php
+++ functions.php
@@ -37,6 +37,11 @@ function aittooja_files() {
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('aittooja_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('aittooja_extra_styles', get_theme_file_uri('/build/index.css'));
+
+    wp_localize_script('main-aittooja-js', 'aittoojaData', array(
+        'root_url' => get_site_url(), 
+    ));
+
 }
 
 add_action('wp_enqueue_scripts', 'aittooja_files');
