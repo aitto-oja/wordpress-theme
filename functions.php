37c9a20e208e6b715e97a76939a5491bcb7d7a5e
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-22 15:56:29 +0300

    Remove html tags from notes

--- functions.php
+++ functions.php
@@ -233,6 +233,12 @@ add_filter('login_headertitle', 'myLoginTitle');
 
 // Force note posts to be private
 function makeNotePrivate($data) {
+
+    if ($data['post_type'] == 'note') {
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+        $data['post_title'] = sanitize_text_field($data['post_title']);
+    }
+
     if ($data['post_type'] == 'note' AND $data['post_type'] != 'trash') {
         $data['post_status'] = "private";
     }
