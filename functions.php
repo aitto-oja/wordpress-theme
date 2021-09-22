4aeacd87ebd52e64ea1bcf064383b07538eccc26
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-22 12:28:03 +0300

    Customize login page

--- functions.php
+++ functions.php
@@ -188,3 +188,26 @@ function noSubsAdminBar() {
 }
 
 add_action('wp_loaded', 'noSubsAdminBar');
+
+// Customize Login Screen
+function myHeaderUrl() {
+    return esc_url(site_url('/'));
+}
+
+add_filter('login_headerurl', 'myHeaderUrl');
+
+function myLoginCSS() {
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('aittooja_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('aittooja_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_action('login_enqueue_scripts', 'myLoginCSS');
+
+// Customize Login Screen Logo
+function myLoginTitle() {
+    return get_bloginfo('name');
+}
+
+add_filter('login_headertitle', 'myLoginTitle');
