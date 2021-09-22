0db75e79b630ff391fb8ffe67d1b1fc975c87d34
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-22 10:03:08 +0300

    Edit the search form

--- page-search.php
+++ page-search.php
@@ -49,9 +49,12 @@ while(have_posts()) {
         <?php } ?>
 
         <div class="generic-content">
-            <form method="get" action="<?php echo esc_url(site_url('/')); ?>">
-                <input type="search" name="s">
-                <input type="submit" value="Search">
+            <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+                <label class="headline headline--medium" for="s">Perform a New Search:</label>
+                <div class="search-form-row">
+                    <input placeholder="What are you looking for?" class="s" id="s" type="search" name="s">
+                    <input class="search-submit" type="submit" value="Search">
+                </div>
             </form>
         </div>
     </div>
