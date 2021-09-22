04ab925480fcaca2416980aa74a68e06b0778627
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-22 10:46:56 +0300

    Use template parts and searchform

--- search.php
+++ search.php
@@ -2,29 +2,25 @@
 
 get_header(); 
 pageBanner(array(
-    'title' => 'Welcome to my blog!', 
-    'subtitle' => 'My thoughts about working with code. ', 
+    'title' => 'Search Results!', 
+    'subtitle' => 'You searched for &ldquo;' . esc_html(get_search_query(false)) . '&rdquo;', 
 ));
 ?>
 
 <div class="container container--narrow page-section">
     <?php 
-        while (have_posts()) {
-            the_post(); ?>
-            <div class="post-item">
-                <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <div class="metabox">
-                    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('j.n.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
-                </div>
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post(); 
+                // get template part: 'template-parts/content-postType'
+                get_template_part('template-parts/content', get_post_type());                
+            }
+            echo paginate_links();
+        } else {
+            echo '<h2 class="headline headline--small-plus">No results match that search.</h2>';
+        }
 
-                <div class="generic-content">
-                    <?php the_excerpt(); ?>
-                    <p><a class="btn btn--blue" href="<?php echo the_permalink(); ?>">Continue reading &raquo;</a></p>
-                </div>
-            </div>
-        <?php }
-
-        echo paginate_links();
+        get_search_form();
     ?>
 </div>
 
