d2b947eceea58af28812ae30202be5455c618762
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-22 14:52:43 +0300

    Edit methods to find new notes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '9271e4b78e0e1711038fc1241119da18');
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '509d54402025dcac871e995bf0d0dee2');
