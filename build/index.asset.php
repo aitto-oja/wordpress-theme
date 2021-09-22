5f338db0ad853b106736bae4676f4fbcb99fab2d
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-22 14:16:24 +0300

    Add edit and update methods to note

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => 'eed9db751fbf95609c9b1141180405cb');
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '60d9956a0a3f66aaaba2b350fbc5161c');
