c046133031de6cb2572a634e92d965164c311371
Reeta Aitto-oja <reeta.m.aittooja@gmail.com>
2021-09-22 12:11:25 +0300

    Redirect subscriber and hide admin bar

--- functions.php
+++ functions.php
@@ -167,3 +167,24 @@ function aittooja_adjust_queries($query) {
 }
 
 add_action('pre_get_posts', 'aittooja_adjust_queries');
+
+
+// Redirect subscriber accounts out of admin and onto homopage
+function redirectSubsToFrontend() {
+    $ourCurrentUser = wp_get_current_user();
+    if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+        wp_redirect(site_url('/'));
+        exit;
+    }
+}
+
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function noSubsAdminBar() {
+    $ourCurrentUser = wp_get_current_user();
+    if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+        show_admin_bar(false);
+    }
+}
+
+add_action('wp_loaded', 'noSubsAdminBar');
